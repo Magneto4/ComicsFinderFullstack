15fba00b63f72e49a8e05264876f0baa81e1fe7e
nseniak <noamseniak@gmail.com>
2023-07-08 16:32:24 +0200

    testing something

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,13 +5,13 @@ import getList from './getList';
 export class AppService implements OnModuleInit {
 	onModuleInit() {
 		console.log(`Initialization...`);
-		getList("Marvel_Staff/Writers");
-		getList("Marvel_Staff/Pencilers");
-		getList("Marvel_Staff/Inkers");
-		getList("Marvel_Staff/Colorists");
-		getList("Marvel_Staff/Letterers");
-		getList("Marvel_Staff/Editors");
-		getList("Characters");
+		// getList("Marvel_Staff/Writers");
+		// getList("Marvel_Staff/Pencilers");
+		// getList("Marvel_Staff/Inkers");
+		// getList("Marvel_Staff/Colorists");
+		// getList("Marvel_Staff/Letterers");
+		// getList("Marvel_Staff/Editors");
+		// getList("Characters");
 	}
 	getHello(): string {
 		return 'Hello World!';
