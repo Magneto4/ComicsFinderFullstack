b8fb91f517bfd51240ca7fd56515688f006d1855
nseniak <noamseniak@gmail.com>
2023-07-10 17:50:18 +0200

    added a router

--- client/src/js/App.js
+++ client/src/js/App.js
@@ -2,13 +2,33 @@ import UserInput from "./input";
 import "../css/App.css"
 import Banner from "./Banner";
 import { useEffect } from "react";
+import { createBrowserRouter, RouterProvider, Navigate } from "react-router-dom";
+
+function Home () {
+	return (<div class="app">
+		<Banner />
+		<UserInput />
+	</div>)
+}
 
 export default function App() {
 	useEffect(() => {
 		document.title = 'Comics finder';
 	}, []);
-	return (<div class="app">
-		<Banner />
-		<UserInput />
-	</div>)
+
+	const router = createBrowserRouter([
+		{
+			path: "/",
+			element: <Navigate to="/home"/>
+		},
+		{
+			path: "/home",
+			element: <Home/>
+		}
+	])
+	return (
+		<div className="app">
+			<RouterProvider router={router}/>
+		</div>
+	)
 }
