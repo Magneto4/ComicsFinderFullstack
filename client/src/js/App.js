b2964025e1dd2d0172b4e3f7925b99683b6b6a8d
nseniak <noamseniak@gmail.com>
2023-07-11 13:54:38 +0200

    removed routing

--- client/src/js/App.js
+++ client/src/js/App.js
@@ -1,36 +1,23 @@
 import UserInput from "./input";
 import "../css/App.css"
 import Banner from "./Banner";
-import { useEffect } from "react";
-import { createBrowserRouter, RouterProvider, Navigate } from "react-router-dom";
 import {Helmet} from "react-helmet";
 
-function Home () {
-	return (<div class="app">
-		<Banner />
-		<UserInput />
-	</div>)
-}
-
 export default function App() {
-	const router = createBrowserRouter([
-		{
-			path: "/",
-			element: <Navigate to="/home"/>
-		},
-		{
-			path: "/home",
-			element: <Home/>
-		}
-	])
 	return (
 		<div className="app">
 			<Helmet>
 				<title>Comics finder</title>
+				<meta property="description" content="Search comic books by criteria easily! Woohoo!"/>
 				<meta property="og:title" content="Comics finder"/>
-				<meta property="og:description" content="Search comic books by criteria easily! Woohoo!"></meta>
+				<meta property="og:description" content="Search comic books by criteria easily! Woohoo!"/>
+				<meta property="og:type" content="website"/>
+				<meta property="og:image" content="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Marvel_Logo.svg/800px-Marvel_Logo.svg.png"/>
 			</Helmet>
-			<RouterProvider router={router}/>
+			<div class="app">
+				<Banner />
+				<UserInput />
+			</div>
 		</div>
 	)
 }
